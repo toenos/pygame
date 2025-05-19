f8a2a198a712ea0ca2a0605a8720d15e5f23a080
toenos <martijnvanerp@hotmail.com>
2025-05-19 19:42:11 +0200

    new files

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print("Screen width:" , SCREEN_WIDTH)
-    print("Screen height:" , SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.Surface.fill(0,0,0)
+        screen.fill("black")
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
