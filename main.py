54d5b60e845fc552425c9820bef86963f8ce56ad
toenos <martijnvanerp@hotmail.com>
2025-05-22 20:55:46 +0200

    asteroid

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import pygame
 from constants import *
 from player import Player
+<<<<<<< Updated upstream
+=======
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+>>>>>>> Stashed changes
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+<<<<<<< Updated upstream
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+=======
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+>>>>>>> Stashed changes
 
     Player.containers = (updatable, drawable)
 
@@ -22,13 +39,26 @@ def main():
                 return
 
         updatable.update(dt)
+<<<<<<< Updated upstream
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
         pygame.display.flip()
 
         #60 FPS
+=======
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+>>>>>>> Stashed changes
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
